62a8fd602d4ebedb92bd4a4e2b54fda0effe3626
Ryulth <adboy94@khu.ac.kr>
2019-01-07 14:31:30 +0900

    mysql & jpa success and api function

--- springboot/src/main/java/com/template/controller/JpaController.java
+++ springboot/src/main/java/com/template/controller/JpaController.java
@@ -4,10 +4,10 @@ import com.template.model.City;
 import com.template.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @RestController
@@ -16,16 +16,45 @@ public class JpaController {
     @Autowired
     private CityRepository cityRepository;
 
-    @RequestMapping("/add")
-    public City add(City city){
+    @GetMapping("/all")
+    public List<City> list(Model model) {
+        List<City> cityList = new ArrayList<>();
+        cityRepository.findAll().forEach(cityList::add); // repository 반환값이 이터레티어 임
+        return cityList;
+    }
+
+    @GetMapping("/add")
+    public City addGet(City city) {
+        System.out.println(city);
         City cityData = cityRepository.save(city);
         return cityData;
     }
 
-    @RequestMapping("/all")
-    public List<City> list(Model model){
-        List<City> cityList = new ArrayList<>();
-        cityRepository.findAll().forEach(cityList::add);
-        return cityList;
+    @PostMapping("/add")
+    public City addPost(@RequestBody City city) {
+        System.out.println(city);
+        City cityData = cityRepository.save(city);
+        return cityData;
+    }
+
+    @PostMapping("/update")
+    public City updatePost(@RequestParam(value = "id") Long id, @RequestBody City city) {
+        System.out.println(city);
+        City cityData = cityRepository.findById(id)
+                .orElse(null);
+        cityData.setName(city.getName());
+        cityData.setState(city.getState());
+        cityData.setUpdateTime(Calendar.getInstance());
+        cityRepository.save(cityData);
+        return cityData;
+    }
+
+    @GetMapping("/delete")
+    public City deletePost(@RequestParam(value = "id") Long id) {
+        System.out.println(id);
+        City cityData = cityRepository.findById(id)
+                .orElse(null);
+        cityRepository.deleteById(id);
+        return cityData;
     }
 }
--- springboot/src/main/java/com/template/controller/PostController.java
+++ springboot/src/main/java/com/template/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.template.controller;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostController {
     @PostMapping(value = "post")
-    public JsonObject response(@RequestBody JsonObject payload){ // JSON 타입으로 바로 변환하면 오류가 가끔 남
+    public String resopnse(@RequestBody String payload) { // JSON 타입으로 바로 변환하면 오류가 가끔 남
         System.out.println(payload);
-        JsonObject res=new JsonObject();
-        res.addProperty("STATUS","SUCCESS");
-        res.addProperty("Content","good");
-        res.addProperty("Request","test");
-        return res;
+        Gson gson = new Gson();
+        JsonObject res = new JsonObject();
+        res.addProperty("text", "ddd");
+        res.addProperty("statue", "good");
+        res.addProperty("todo", "wod");
+        return gson.toJson(res);
     }
 }
